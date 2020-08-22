ac4fa66eac7f26f56e0fe24d311b51a040cd348b
udeogu ifechuwku <ifebrand6@gmail.com>
2020-08-22 23:15:15 +0100

    added Blog

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,89 @@
 <template>
- <div>
-   <app-header v-bind:title="title" v-on:changeTitle='update($event)'></app-header>
+  <div>
+    <!-- FORM HELPER -->
+    <!-- <formHelper>
+      <h1 slot="header">Form description</h1>
+      <p slot="p">form paragraph {{title}}</p>
+    </formHelper> -->
+
+    <!-- PASSING EVENT TO CHILD -->
+    <!-- <app-header v-bind:title="title" v-on:changeTitle='update($event)'></app-header>
    <app-main v-bind:ninjas='ninjas'></app-main>
-   <app-footer v-bind:title="title" v-on:changeTitle='title=$event'></app-footer>
- </div>
+   <app-footer v-bind:title="title" v-on:changeTitle='title=$event'></app-footer> -->
+
+   <!-- DYNAMIC COMPONENTS -->
+    <!-- <keep-alive>
+      <component v-bind:is='components'></component>
+    </keep-alive>
+
+    <button v-on:click="components = 'form-one'">use form one</button>
+    <button v-on:click="components = 'form-two'">use form two</button> -->
+
+    <!-- BLOG -->
+    <keep-alive>
+      <add-blog></add-blog>
+    </keep-alive>
+
+  </div>
 </template>
 
 <script>
-export default {
-  data(){
-    return {
-    ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-    ],
-    title: `People's Tracker`,
+  // import formHelper from './components/formHelper'
+  // import formOne from './components/formOne'
+  // import formTwo from './components/formTwo'
+    import addBlog from './components/addBlog'
+  export default {
+    components: {
+      // 'formHelper': formHelper
+      // 'form-one': formOne,
+      // 'form-two': formTwo
+          'add-blog': addBlog
+    },
+    data() {
+      return {
+        ninjas: [{
+            name: 'Ryu',
+            speciality: 'Vue Components',
+            show: false
+          },
+          {
+            name: 'Crystal',
+            speciality: 'HTML Wizardry',
+            show: false
+          },
+          {
+            name: 'Hitoshi',
+            speciality: 'Click Events',
+            show: false
+          },
+          {
+            name: 'Tango',
+            speciality: 'Conditionals',
+            show: false
+          },
+          {
+            name: 'Kami',
+            speciality: 'Webpack',
+            show: false
+          },
+          {
+            name: 'Yoshi',
+            speciality: 'Data Diggin',
+            show: false
+          }
+        ],
+        title: `People's Tracker`,
+        components: 'form-one'
 
+      }
+    },
+    methods: {
+      update(d) {
+        this.title = d
+      }
     }
-  },
-  methods: {
-    update(d){
-     this.title = d
-    }
+
   }
-
-}
 </script>
 
 <style scoped>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="add-blog">
+        <h2>Add a New Blog Post</h2>
+        <form>
+            <label>Blog Title:</label>
+            <input type="text" v-model.lazy="blog.title" required />
+            <label>Blog Content:</label>
+            <textarea v-model.lazy.trim="blog.content"></textarea>
+            <div id="check-box">
+                <span v-for="(category,index) in categories" :key=index>
+                    <label> {{category}}</label>
+                    <input type="checkbox" v-bind:value='category' v-model='blog.categories'>
+                </span>
+            </div>
+        </form>
+        <div id="preview">
+            <h3>Preview blog</h3>
+            <p>Blog title: {{ blog.title }}</p>
+            <p>Blog content:</p>
+            <p style="white-space: pre">{{ blog.content }}</p>
+            <p>Blog Category</p>
+            <ul>
+                <li v-for="(category,index) in blog.categories" :key=index>{{category}}</li>
+            </ul>  
+        </div>
+    </div>
+</template>
+
+<script>
+// Imports
+export default {
+    data () {
+        return {
+            blog: {
+                title: '',
+                content: '',
+                categories: []
+            },
+            categories: ["Art", 'Science', 'Technology', 'Business']
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+#add-blog *{
+    box-sizing: border-box;
+}
+#add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+h3{
+    margin-top: 10px;
+}
+
+#check-box input{
+    display: inline-block;
+    margin-right: 10px;
+
+ }
+#check-box label{
+        display: inline-block;
+ }
+</style>
